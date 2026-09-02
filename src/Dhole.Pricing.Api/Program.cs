907248833d3dd32856b466f465af857e6f0107c0
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-02 12:08:58 -0600

    feat(pricing): map LCL endpoints

--- src/Dhole.Pricing.Api/Program.cs
+++ src/Dhole.Pricing.Api/Program.cs
@@ -115,6 +115,7 @@ app.MapCommercialTermEndpoints();
 app.MapPricingConfigCatalogEndpoints();
 app.MapDataExtractionImportEndpoints();
 app.MapLogisticsNewsEndpoints();
+app.MapLclEndpoints();
 
 using (var scope = app.Services.CreateScope())
 {
