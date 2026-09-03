2d87562ab4720916e2d0a910e254858829d3a676
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-02 23:22:25 -0600

    feat(pricing): add own LCL consolidation create scope

--- src/Dhole.Pricing.Api/Authorization/PricingScopeName.cs
+++ src/Dhole.Pricing.Api/Authorization/PricingScopeName.cs
@@ -29,6 +29,9 @@ internal static class PricingScopeNames
     public const string ImportFclRateReject = "pricing.import-fcl-rate.reject";
     public const string ImportFclRateCreateAsRate = "pricing.import-fcl-rate.create-as-rate";
 
+    // Own LCL consolidations
+    public const string OwnLclConsolidationCreate = "pricing.own-lcl-consolidation.create";
+
     // Rates
     public const string RateCreate = "pricing.rate.create";
     public const string RateView = "pricing.rate.view";
